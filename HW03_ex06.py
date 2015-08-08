598e573c2f3194c7bce06d45674d2a29465f8bb4
Astika <astikagupta@gmail.com>
2015-08-07 17:11:22 -0700

    HW03 astika

--- HW03_ex06.py
+++ HW03_ex06.py
@@ -6,8 +6,14 @@
 ################################################################################
 # Exercise 1
 # When you submit only include your final function: compare
-
-
+import math
+def compare(x,y):
+   if x<y:
+     return 1
+   elif x==y:
+    return 0
+   else:
+    return -1
 
 
 
@@ -15,6 +21,9 @@
 # Exercise 2
 # When you submit only include your final function: hypotenuse
 # Do develop incrementally. Do not share here.
+def hypotenuse(x,y):
+  z = math.sqrt(x**2+y**2)
+  return z
 
 
 
@@ -23,7 +32,11 @@
 ################################################################################
 # Exercise 3
 # When you submit only include your final function: is_between
-
+def is_between(x,y,z):
+  if y-x==z-y:
+    return True
+  else:
+    return False 
 
 
 
@@ -32,6 +45,38 @@
 # Exercise 6
 # When you submit only include your final function: is_palindrome
 
+# def is_palindrome(s):
+#    
+#     x=s[::-1]
+#     if x==s:
+#       return True
+#     else:
+#       return False
+
+
+def first(word):
+
+    return word[0]
+
+
+def last(word):
+    
+    return word[-1]
+
+
+def middle(word):
+
+    return word[1:-1]
+
+
+def is_palindrome(word):
+   
+    if len(word) <= 1:
+        return True
+    if first(word) != last(word):
+        return False
+    return is_palindrome(middle(word))
+ 
 
 
 
@@ -39,10 +84,23 @@
 ################################################################################
 # Exercise 7
 # When you submit only include your final function: is_power
+def is_power(x,y):
+  if x%y==0 and (x/y)%y==0:
+    return True
+  else:
+    return False
 
-
-
-
+# using recursion
+def is_power(x,y):
+   m=x/y
+   if x == 1:
+     return True
+   elif m==1:
+     return True
+   elif m%y==0 and x%y==0:
+     return is_power(m,y)
+   else:
+     return False
 
 ################################################################################
 def main():
@@ -60,28 +118,28 @@ def main():
     ############################################################################
     # Uncomment the below to test and before commiting:
     # # Exercise 1
-    # compare(1,1)
-    # compare(1,2)
-    # compare(2,1)
+    # print compare(1,1)
+    # print compare(1,2)
+    # print compare(2,1)
     # # Exercise 2
-    # hypotenuse(1,1)
-    # hypotenuse(3,4)
-    # hypotenuse(1.2,12)
+    # print hypotenuse(1,1)
+    # print hypotenuse(3,4)
+    # print hypotenuse(1.2,12)
     # # Exercise 3
-    # is_between(1,2,3)
-    # is_between(2,1,3)
-    # is_between(3,1,2)
-    # is_between(1,1,2)
+    # print is_between(1,2,3)
+    # print is_between(2,1,3)
+    # print is_between(3,1,2)
+    # print is_between(1,1,2)
     # # Exercise 6
-    # is_palindrome("Python")
-    # is_palindrome("evitative")
-    # is_palindrome("sememes")
-    # is_palindrome("oooooooooooo")
+    #  print is_palindrome("Python")
+    # print is_palindrome("evitative")
+    # print is_palindrome("sememes")
+    # print is_palindrome("oooooooooooo")
     # # Exercise 7
-    # is_power(28,3)
-    # is_power(27,3)
-    # is_power(248832,12)
-    # is_power(248844,12)
+    # print is_power(28,3)
+    #  print is_power(27,3)
+    #   print is_power(248832,12)
+      print is_power(248844,12)
 
 
 if __name__ == "__main__":
--- HW03_string1.py
+++ HW03_string1.py
@@ -26,7 +26,12 @@
 # and donuts(23) returns 'Number of donuts: many'
 def donuts(count):
   # +++your code here+++
-  return
+   if count < 10:
+     return("Number of donuts:"+ str(count))
+   else:
+     return("Number of donuts : many")
+    
+  
 
 
 # B. both_ends
@@ -85,27 +90,27 @@ def main():
   test(donuts(10), 'Number of donuts: many')
   test(donuts(99), 'Number of donuts: many')
 
-  print
-  print 'both_ends'
-  test(both_ends('spring'), 'spng')
-  test(both_ends('Hello'), 'Helo')
-  test(both_ends('a'), '')
-  test(both_ends('xyz'), 'xyyz')
-
-  
-  print
-  print 'fix_start'
-  test(fix_start('babble'), 'ba**le')
-  test(fix_start('aardvark'), 'a*rdv*rk')
-  test(fix_start('google'), 'goo*le')
-  test(fix_start('donut'), 'donut')
-
-  print
-  print 'mix_up'
-  test(mix_up('mix', 'pod'), 'pox mid')
-  test(mix_up('dog', 'dinner'), 'dig donner')
-  test(mix_up('gnash', 'sport'), 'spash gnort')
-  test(mix_up('pezzy', 'firm'), 'fizzy perm')
+#   print
+#   print 'both_ends'
+#   test(both_ends('spring'), 'spng')
+#   test(both_ends('Hello'), 'Helo')
+#   test(both_ends('a'), '')
+#   test(both_ends('xyz'), 'xyyz')
+# 
+#   
+#   print
+#   print 'fix_start'
+#   test(fix_start('babble'), 'ba**le')
+#   test(fix_start('aardvark'), 'a*rdv*rk')
+#   test(fix_start('google'), 'goo*le')
+#   test(fix_start('donut'), 'donut')
+# 
+#   print
+#   print 'mix_up'
+#   test(mix_up('mix', 'pod'), 'pox mid')
+#   test(mix_up('dog', 'dinner'), 'dig donner')
+#   test(mix_up('gnash', 'sport'), 'spash gnort')
+#   test(mix_up('pezzy', 'firm'), 'fizzy perm')
 
 
 # Standard boilerplate to call the main() function.
